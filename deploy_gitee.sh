9d7ff5e895f1c78b0c854d512139806e9a2c1b94
dream同学0 <1399541701@qq.com>
2024-02-17 12:52:17 +0800

    deploy

--- deploy_gitee.sh
+++ deploy_gitee.sh
@@ -3,3 +3,6 @@ git add -A
 git commit -m 'deploy'
 git remote add origin_gitee git@gitee.com:o19859860010/csu_stte_files.git
 git push origin_gitee main
+
+exec /bin/bash
+
--- deploy_github.sh
+++ deploy_github.sh
@@ -3,3 +3,4 @@ git add -A
 git commit -m 'deploy'
 git remote add origin_github git@github.com:dream-oyh/csu_stte_files.git
 git push origin_github main
+exec /bin/bash
