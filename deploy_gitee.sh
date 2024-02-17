3cdcfaa6a898602081b04525680423a575fe8180
dream同学0 <1399541701@qq.com>
2024-02-17 12:57:15 +0800

    deploy:20240217-12:57:15

--- deploy_gitee.sh
+++ deploy_gitee.sh
@@ -1,6 +1,6 @@
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy:'$(date "+%Y%m%d-%H:%M:%S")
 git remote add origin_gitee git@gitee.com:o19859860010/csu_stte_files.git
 git push origin_gitee main
 
--- deploy_github.sh
+++ deploy_github.sh
@@ -1,6 +1,6 @@
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy:'$(date "+%Y%m%d-%H:%M:%S")
 git remote add origin_github git@github.com:dream-oyh/csu_stte_files.git
 git push origin_github main
 exec /bin/bash
